2a3f07c01eb092568e4712179ca8955e0729311e
Mikro Team <mikroangkot@gmail.com>
2026-06-09 21:32:52 +0700

    Add FormatUI class with snackbar functionality

---
+++ helpers/src/custom_ui.py
@@ -0,0 +1,19 @@
+import flet as ft
+
+class FormatUI:
+  
+  @staticmethod
+  def show_snackbar(page, message: str, warna: str):
+    color_value = getattr(ft.Colors, warna, ft.Colors.YELLOW_900)
+    snak_bar = ft.SnackBar(
+      content = ft.Text(
+        message,
+        color = ft.Colors.WHITE,
+        weight = ft.FontWeight.BOLD,
+        text_align = ft.TextAlign.CENTER
+      ),
+      bgcolor = ft.Colors.with_opacity(0.5, color_value)
+    )
+    page.overlay.append(snack_bar)
+    snack_bar.open = True
+    page.update()
