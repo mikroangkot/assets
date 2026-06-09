2a8f11a76b16be08b2d8b0a16e8eb6dba3255876
Mikro Team <mikroangkot@gmail.com>
2026-06-09 21:48:31 +0700

    Reorder inheritance in DataFormatter class

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -3,8 +3,8 @@ from .src.custom_dispatch import FormatDispatch
 from .src.custom_ui import FormatUI
 
 class DataFormatter(
-  FormatUI,
   FormatDatetime,
-  FormatDispatch
+  FormatDispatch,
+  FormatUI
 ):
   pass
