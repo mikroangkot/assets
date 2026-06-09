60602567ef375200ef1453be2fde48149acd7253
Mikro Team <mikroangkot@gmail.com>
2026-06-09 21:42:59 +0700

    Refactor DataFormatter to inherit from multiple classes

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,1 +1,10 @@
-from .dataformatter import DataFormatter
+from .src.custom_ui import FormatUI
+from .src.custom_datetime import FormatDatetime
+from .src.custom_dispatch import FormatDispatch
+
+class DataFormatter(
+  FormatUI,
+  FormatDatetime,
+  FormatDispatch
+):
+  pass
