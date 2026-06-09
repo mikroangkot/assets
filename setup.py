21715f29316d2e31cd9f4f4b00b23029dc6affe7
Mikro Team <mikroangkot@gmail.com>
2026-06-09 22:51:08 +0700

    Enhance setup.py with package metadata
    Added metadata fields including description, URL, author, author email, and license.

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
   packages=find_packages(),
   install_requires=[
     "flet"
-  ]
+  ],
+  description="Package helpers to Mikro Monitor apk",
+  url="https://github.com/mikroangkot/assets",
+  author="RumahDjigo",
+  author_email="rumahdjigo@gmail.com",
+  license="MIT"
 )
