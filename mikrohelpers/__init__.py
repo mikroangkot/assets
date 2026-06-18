0f49a9d75e9a8b1dc611d368352e1839408a411f
Mikro Team <mikroangkot@gmail.com>
2026-06-18 19:10:57 +0700

    Add DataFormatter class that inherits multiple formats

--- mikrohelpers/__init__.py
+++ mikrohelpers/__init__.py
@@ -1,1 +1,12 @@
+from .src.custom_datetime import FormatDatetime
+from .src.custom_dispatch import FormatDispatch
+from .src.custom_ui import FormatUI
+from .src.custom_decryptenv import DecryptData
 
+class DataFormatter(
+  FormatDatetime,
+  FormatDispatch,
+  FormatUI,
+  DecryptData
+):
+  pass
