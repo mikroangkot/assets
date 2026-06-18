ed4514877f3489d9e2342d04721ecc4f7801cca1
Mikro Team <mikroangkot@gmail.com>
2026-06-18 20:08:30 +0700

    Update setup.py to specify package directory

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup, find_packages
 setup(
   name="mikrohelpers",
   version="0.6",
-  packages=find_packages(),
-  install_requires=[
-    "flet",
-    "bcrypt",
-    "python-dotenv",
-    "cryptography"
-  ],
   description="Package helpers to format intern data",
   url="https://github.com/mikroangkot/assets",
   author="RumahDjigo",
   author_email="rumahdjigo@gmail.com",
   license="MIT"
+  packages=find_packages(where="src"),
+  packages_dir={"": "src"},
+  install_requires=[
+    "flet",
+    "bcrypt",
+    "python-dotenv",
+    "cryptography"
+  ]
 )
