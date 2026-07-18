2e0c3543f3253994c313ff28c3c35a9c1ad6cf1d
Mikro Team <mikroangkot@gmail.com>
2026-07-18 23:35:47 +0700

    Add AppStorage to DataFormatter class

--- src/mikrohelpers/__init__.py
+++ src/mikrohelpers/__init__.py
@@ -3,12 +3,14 @@ from .custom_dispatch import FormatDispatch
 from .custom_ui import FormatUI
 from .custom_decryptenv import DecryptData
 from .custom_log import DataLog
+from .custom_sharedpreferences import AppStorage
 
 class DataFormatter(
   FormatDatetime,
   FormatDispatch,
   FormatUI,
   DecryptData,
-  DataLog
+  DataLog,
+  AppStorage
 ):
   pass
