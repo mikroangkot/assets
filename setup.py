08f722008c3ca4cb4ebbe875039b0b498a7cc3d0
Mikro Team <mikroangkot@gmail.com>
2026-08-12 13:04:21 +0700

    Bump version from 1.1.0 to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="mikrohelpers",
-  version="1.1.0",
+  version="1.1.1",
   description="Package helpers to format intern data",
   url="https://github.com/mikroangkot/assets",
   author="RumahDjigo",
